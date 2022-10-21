38b9d3a4d2caae65e18b7eef2300d0b18a6744f2
motyotyotyo <bence.mattyasovszky@stud.uni-corvinus.hu>
2022-10-21 10:18:18 +0200

    webszolgaltatas hivasa

--- Valuta/Valuta/Form1.cs
+++ Valuta/Valuta/Form1.cs
@@ -12,9 +12,25 @@ namespace Valuta
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            diFeladat();            
+        }
+
+        private void diFeladat()
+        {
+            var mnbService = new MnbServiceReference.MNBArfolyamServiceSoapClient();
+
+            var request = new MnbServiceReference.GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
