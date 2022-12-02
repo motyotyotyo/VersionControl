f4ff04a57d89c93040e1a8e38d032904aafa1096
motyotyotyo <bence.mattyasovszky@stud.uni-corvinus.hu>
2022-12-02 11:03:28 +0100

    Creating Simulation Steps

--- MikroSzim/MikroSzim/Form1.cs
+++ MikroSzim/MikroSzim/Form1.cs
@@ -97,5 +97,31 @@ namespace MikroSzim
             }
             return deathProbabilities;
         }
+        private void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+            byte age = (byte)(year - person.BirthYear);
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.ProbabilityofDeath).FirstOrDefault();
+            if (rng.NextDouble() <=pDeath)
+            {
+                person.IsAlive = false;
+            }
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.ProbabiltyofBirth).FirstOrDefault();
+                if (rng.NextDouble() <=pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
+        }
     }
 }
