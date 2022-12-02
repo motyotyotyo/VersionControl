ddb4a6d69ad61c85badc626be6367aec5ca8cc07
motyotyotyo <bence.mattyasovszky@stud.uni-corvinus.hu>
2022-12-02 10:43:00 +0100

    Creating function to get data to list

--- MikroSzim/MikroSzim/Entities/BirthProbability.cs
+++ MikroSzim/MikroSzim/Entities/BirthProbability.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MikroSzim.Entities
 {
-    class BirthProbability
+    public class BirthProbability
     {
         public int Age { get; set; }
         public int NbrOfChildren { get; set; }
