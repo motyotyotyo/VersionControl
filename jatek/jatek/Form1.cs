566337f5c227363f2251fc3c515ce082ea2012af
motyotyotyo <bence.mattyasovszky@stud.uni-corvinus.hu>
2022-12-09 10:17:53 +0100

    Game population creation

--- jatek/jatek/Form1.cs
+++ jatek/jatek/Form1.cs
@@ -15,14 +15,21 @@ namespace jatek
     {
         GameController gc = new GameController();
         GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
-            gc.AddPlayer();
-            gc.Start(true);
+            for (int i = 0; i < populationSize-1; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
         }
     }
 }
