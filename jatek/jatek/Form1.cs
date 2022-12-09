f3efa6555983f2c24da4d8d5d942cff0a2500ba1
motyotyotyo <bence.mattyasovszky@stud.uni-corvinus.hu>
2022-12-09 10:58:26 +0100

    Final product

--- jatek/jatek/Form1.cs
+++ jatek/jatek/Form1.cs
@@ -71,8 +71,18 @@ namespace jatek
             {
                 winnerBrain = winners.FirstOrDefault().Brain.Clone();
                 gc.GameOver -= Gc_GameOver;
+                button1.Visible = true;
                 return;
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
         }
     }
 }
